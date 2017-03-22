68bdb9a7e6f60893b0a19ff6df5a03905944703f
lilschmitz <KathrinLSchmitz@gmail.com>
2017-03-22 15:39:44 +0000

    updated host

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogks.herokuapp.com']
 INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
