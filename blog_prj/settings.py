34a94c66d0c4d541919bf7e70295ac9dda786958
lilschmitz <KathrinLSchmitz@gmail.com>
2017-03-22 15:14:53 +0000

    initial commit

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd9=48aojskxs1su&c@*p#4swq&kywybgwdrke_(ca9c&9ps6it'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,7 +46,8 @@ INSTALLED_APPS = [
     'disqus',
     'django_gravatar',
     'django_forms_bootstrap',
-    'debug_toolbar'
+    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
@@ -81,13 +85,14 @@ WSGI_APPLICATION = 'blog_prj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+#3DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  #  }
+#}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES={'default':dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 
 # Password validation
@@ -141,5 +146,5 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DISQUS_WEBSITE_SHORTNAME = 'richardciblog'
+DISQUS_WEBSITE_SHORTNAME = 'KathrinBlog'
 SITE_ID = 1
